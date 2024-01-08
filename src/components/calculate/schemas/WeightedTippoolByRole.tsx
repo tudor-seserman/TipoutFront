c120d64fff9b265c3b700c527513bee60ba3d569
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-01-08 12:39:36 -0600

    Better Described WeightedTippoolByRole

--- src/components/calculate/schemas/WeightedTippoolByRole.tsx
+++ src/components/calculate/schemas/WeightedTippoolByRole.tsx
@@ -26,21 +26,7 @@ const WeightedTippoolByRole = ({ handleSubmit, moneyHandlers, setMoneyHandlers, 
 
   return (
     <>
-      <div>
-        {tipRates.map(function (tipRate, index) {
-          return (
-            <h5 key={index}>
-              {tipRate.roleName}: {tipRate.tipRate}%
-            </h5>
-          );
-        })}
-      </div>
-      <Link to="/settings">
-        <Button>Customize role distribution </Button>
-      </Link>
-      <br />
-      <br />
-      <br />
+
       <Form onSubmit={handleSubmit}>
         <div>
           <h3>Enter Tips</h3>
@@ -101,6 +87,40 @@ const WeightedTippoolByRole = ({ handleSubmit, moneyHandlers, setMoneyHandlers, 
           />
         </div>
       </Form>
+      <br />
+      <br />
+      <h5>Employee Tipout = Total Tips <strong>multiped by</strong> Weighed Assigned to Each Role Included in the Tippool <strong>over</strong> Total Weight of Roles Included In the Tippool <strong>divided by</strong> Number of Employees of that Role in the Tippool</h5>
+
+      <p>Tips are collected and then divide by the weight given to the roles in the tippool.
+        The tips from each role are than divided equally by employees in that role.
+      </p><p>
+        For example:
+        If the server role is weighted at 40 and the busser is weighted at 10,
+        and there are 2 servers in the tippool and one busser.
+      </p><p>
+        <strong>Server Tipout = $40 = $100 * (40/50) ÷ 2</strong>
+      </p><p>
+        If the total tips collected are $100: the servers would split $80($40 a piece) and the busser would receive $20.
+      </p><p>
+        If a bartender role was weighted at 50 and one was added to the tippool of $100:
+        Servers would split $40($20 a piece)
+        Busser would receive $20
+        Bartender would receive $50
+      </p><p>
+        <strong>Server Tipout = $20 = $100 * (40/100) ÷ 2</strong>
+      </p>
+      <div>
+        {tipRates.map(function (tipRate, index) {
+          return (
+            <h5 key={index}>
+              {tipRate.roleName + "s"}: {tipRate.tipRate}
+            </h5>
+          );
+        })}
+      </div>
+      <Link to="/settings">
+        <Button>Customize role weight </Button>
+      </Link>
     </>
   );
 };
