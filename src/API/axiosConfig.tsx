2b963e82a24ddc86de90dc72ab084caa5ec72d94
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-01-13 19:01:35 -0600

    Display Reports

--- src/API/axiosConfig.tsx
+++ src/API/axiosConfig.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // DEVELOPMENT
-  baseURL: "http://localhost:8080/",
+  // baseURL: "http://localhost:8080/",
   // PRODUCTION
-  // baseURL: "https://tipoutback.onrender.com",
+  baseURL: "https://tipoutback.onrender.com",
 });
